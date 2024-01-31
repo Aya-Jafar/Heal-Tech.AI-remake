12df5826c2402b8e12db18f1ed723c68ef60540f
Aya-jafar <aya.jafar002@gmail.com>
2024-01-31 21:11:01 +0300

    add copyrights

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,16 @@
 import * as React from "react";
 import logo from "../images/healai-icon.png";
 
-
 export function Footer() {
   return (
     <div className="footer">
       <div className="header">
-      {/* <hr /> */}
+        {/* <hr /> */}
         <div className="logo">
           <img src={logo} alt="" />
-          <h2>Heal Tech.AI</h2>
+          <p>© 2023 HEAL TECH.AI . All rights reserved</p>
         </div>
       </div>
-    
     </div>
   );
 }
