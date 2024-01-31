43f53aeef53b69fd20c7945d2e0a1984f77bb574
Aya-jafar <aya.jafar002@gmail.com>
2024-01-31 11:40:24 +0300

    making services wrapper and routing

---
+++ src/components/ServiceItem.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { linkStyle } from "../dynamicStyles";
+
+
+interface ServiceItemProps {
+  color: string;
+  title: string;
+  description: string;
+  path: string;
+}
+
+export default function ServiceItem(props: ServiceItemProps) {
+
+  return (
+    <div className="service-item" style={{ backgroundColor: props.color }}>
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+      <Link to={props.path} style={{ ...linkStyle, marginTop: "auto" }}>
+        <button className="try-btn" style={{ backgroundColor: props.color }}>
+          <h3>Try Now</h3>
+        </button>
+      </Link>
+    </div>
+  );
+}
--- src/components/ServicesWrapper.tsx
+++ src/components/ServicesWrapper.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import ServiceItem from "./ServiceItem";
 
 interface ServicesWrapperProps {}
 
@@ -6,12 +7,27 @@ export function ServicesWrapper(props: ServicesWrapperProps) {
   return (
     <div className="services">
       <center>
-        <h1>Our Services</h1>
+        <h1 style={{fontSize:"40px"}}>Our Services</h1>
       </center>
       <div className="services-wrapper">
-        <div>item1</div>
-        <div>item2</div>
-        <div>item3</div>
+        <ServiceItem
+          title="Medical Auto Completion"
+          description="Saves time by quickly providing accurate medical text predictions"
+          path="/services/auto-completion"
+          color="#0024AB"
+        />
+        <ServiceItem
+          title="Text summarization"
+          description="Quickly extracts key information from lengthy texts.make faster decisions, and spend less time sifting through data"
+          path="/services/summarization"
+          color="#0546C6"
+        />
+        <ServiceItem
+          title="Medical Chat bot"
+          description="Instantly accessible and knowledgeable, it provides real-time assistance, answers queries, and elevates your communication experience"
+          path="/services/chat-bot"
+          color="#0283CB"
+        />
       </div>
     </div>
   );
