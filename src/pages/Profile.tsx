c407cfdf83504177b617d9fa7a711cc32625db4d
Aya-jafar <aya.jafar002@gmail.com>
2024-03-25 20:56:30 +0300

    Edit profile info section & refactor helper functions

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,8 +3,9 @@ import { useAuth } from "../store/auth";
 import { AuthSchema } from "../schema";
 import { getSavedGeneratedText } from "../Firebase/data";
 import { Avatar } from "@mui/material";
-import { deepOrange } from "@mui/material/colors";
+import { cyan } from "@mui/material/colors";
 import { getUserInfo } from "../Firebase/data";
+import { get1st2Letters } from "../utils/helpers";
 
 interface SavedGenerated {
   title?: string;
@@ -24,27 +25,25 @@ function Profile() {
   // TODO: Remove this state and make the `savedGenerated` dynamic
   const [loaded, setLoaded] = useState<boolean>(false);
 
-
   useEffect(() => {
     getUserInfo().then((result) => setUserInfo(result));
     getSavedGeneratedText(setSavedGenerated, setLoaded);
   }, [currentUser, loaded]);
 
-  const get1st2Letters = () => {
-    return `${(currentUser as any)?.displayName[0]}${
-      (currentUser as any)?.displayName.split(" ")[1][0]
-    }`;
-  };
 
   return (
     <div className="profile">
-      <div className="into-section">
-        <Avatar sx={{ bgcolor: deepOrange[500], width: 100, height: 100 }}>
-          <h1>{get1st2Letters()}</h1>
+      <div className="info-section">
+        <Avatar sx={{ bgcolor: cyan[500], width: 100, height: 100 }}>
+          <h1>{get1st2Letters(currentUser)}</h1>
         </Avatar>
         <div>
-          <h2>{userInfo?.userName}</h2>
-          <h3>{userInfo?.specialization}</h3>
+          <h2>{`Dr. ${(currentUser as any)?.displayName}`}</h2>
+          {userInfo?.specialization ? (
+            <p>{userInfo?.specialization}</p>
+          ) : (
+            <>None</>
+          )}
         </div>
       </div>
 
@@ -64,7 +63,9 @@ function Profile() {
           ))}
         </div>
       ) : (
-        <></>
+        <center>
+          <p>No Saved texts yet...</p>
+        </center>
       )}
     </div>
   );
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,11 @@
 import * as pdfjs from "pdfjs-dist";
 import "pdfjs-dist/build/pdf.worker.min";
 
+/**
+ *
+ * @param file data
+ * @returns extracted text from PDF
+ */
 export const extractPdfText = async (data: string) => {
   const loadingTask = pdfjs.getDocument({ data });
   const pdf = await loadingTask.promise;
@@ -21,6 +26,27 @@ export const extractPdfText = async (data: string) => {
   return text;
 };
 
+/**
+ *
+ * @param currentUser | object
+ * @returns 1st 2 letter from username
+ */
+export const get1st2Letters = (currentUser: object) => {
+  // If username has 2 names
+  if ((currentUser as any)?.displayName.split(" ").length > 1) {
+    return `${(currentUser as any)?.displayName[0]}${
+      (currentUser as any)?.displayName.split(" ")[1][0]
+    }`;
+  }
+  // If username is only one name
+  return `${(currentUser as any)?.displayName[0]}`;
+};
+
+/**
+ *
+ * @param input text for summerization model
+ * @returns total  number of words (tokens)  in that text
+ */
 export const countTokens = (text: string) => {
   let tokensCount = new Map<string, number>();
 
