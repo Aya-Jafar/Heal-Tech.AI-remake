d7b45bfa5bc689c91f172094a912dcaa15b3e250
Aya-jafar <aya.jafar002@gmail.com>
2024-04-20 18:28:49 +0300

    Hotfix: input format in summary model new API

--- src/huggingFace/textSummary.ts
+++ src/huggingFace/textSummary.ts
@@ -1,9 +1,9 @@
-async function summarizeText(text: object) {
-	
+async function summarizeText(text: any) {
+
   const response = await fetch(`${process.env.REACT_APP_SUMMARY_URL}`, {
     headers: { "Content-Type": "application/json" },
     method: "POST",
-    body: JSON.stringify({ long_text: text }),
+    body: JSON.stringify({ long_text: text['inputs'] }),
   });
   const result = await response.json();
   return result;
