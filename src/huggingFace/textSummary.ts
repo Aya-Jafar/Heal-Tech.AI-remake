db9ef18fa0cb8a73466d8b60b19db5adea7ec76f
Aya-jafar <aya.jafar002@gmail.com>
2024-04-21 06:41:04 +0300

    Hotfix: Validation with the new API structure

--- src/huggingFace/textSummary.ts
+++ src/huggingFace/textSummary.ts
@@ -1,12 +1,17 @@
 async function summarizeText(text: any) {
+  console.log(text, "inside summary");
 
-  const response = await fetch(`${process.env.REACT_APP_SUMMARY_URL}`, {
-    headers: { "Content-Type": "application/json" },
-    method: "POST",
-    body: JSON.stringify({ long_text: text['inputs'] }),
-  });
-  const result = await response.json();
-  return result;
+  if (text && text.inputs && text["inputs"].length > 0) {
+    const response = await fetch(`${process.env.REACT_APP_SUMMARY_URL}`, {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify({ long_text: text["inputs"] }),
+    });
+    const result = await response.json();
+    return result;
+  } else {
+    return null;
+  }
 }
 
 export default summarizeText;
